0e4e96f88412f71a7648021cde21ed6bc4f83264
benne0407 <fanclubbando@gmail.com>
2026-03-24 12:57:52 +0700

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print("Hello Github")
 def la_so_nguyen_to (n):
   if(n<2) : return False
-    for i in range (n):
+    for i in range (2, int (n**0.5 +1):
       if n % i == 0 : return False;
     return True
